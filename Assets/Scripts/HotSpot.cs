6592b3fdfc4ad2d4f9041d5b154d358482e1a874
Unknown <Bilal.Douiyeb@hva.nl>
2019-05-17 14:21:04 +0200

    hotspot onclick

---
+++ Assets/Scripts/HotSpot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class HotSpot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler  {
+
+    public string scene; // :)
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+    public void GobBack()
+    {
+        //singelton.current.LoadScene(scene);
+    }
+
+  
+}
