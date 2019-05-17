e0c4c20a0ec9edb31feeda9f61861c5a7a72a5a8
Unknown <Bilal.Douiyeb@hva.nl>
2019-05-17 13:12:12 +0200

    Slideshow bob :)

---
+++ Assets/Scripts/ImageSlideShow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSlideShow : MonoBehaviour {
+    public   List<Sprite> slideshowImages = new List<Sprite>();
+    private int imageCount;
+	// Use this for initialization
+	void Start () {
+        GetComponentInChildren<Image>().overrideSprite = slideshowImages[imageCount];
+        GetComponentInChildren<Image>().preserveAspect = true;
+    }
+
+    // Update is called once per frame
+    public void ImageSlider(bool next)
+    {
+        if (next)
+        {
+            if(imageCount == slideshowImages.Count - 1)
+            {
+                imageCount = 0;
+            }
+            else imageCount++;
+        }
+        if (!next)
+        {
+            if(imageCount == 0)
+            {
+                imageCount = slideshowImages.Count - 1;
+            }
+
+            else imageCount--;
+        }
+
+        GetComponentInChildren<Image>().overrideSprite = slideshowImages[imageCount];
+
+    }
+}
+
+    
