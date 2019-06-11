8879240dbe939c718316d012e34441cdddcdff0d
Peter Marcelis <peter.marcelis@hva.nl>
2019-06-11 10:34:54 +0200

    ADDED oculus package without large mesh

---
+++ Assets/Scripts/AFKManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using ControllerSellection;
+
+public class AFKManager : MonoBehaviour {
+
+	private void OnEnable() {
+		
+	}
+
+	private void OnDisable() {
+		
+	}
+}
