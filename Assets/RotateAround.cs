ca0f0ea9d2a0611ab02de0bb0efaa3cffc8b7628
Peter Marcelis <peter.marcelis@hva.nl>
2019-06-11 11:47:47 +0200

    Added some pictures

---
+++ Assets/RotateAround.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+	public float speed = 5f;
+
+	private void Update() {
+		transform.RotateAround(Vector3.zero,Vector3.up, speed * Time.deltaTime);
+	}
+}
--- Assets/Scripts/AFKManager.cs
+++ Assets/Scripts/AFKManager.cs
@@ -1,15 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using ControllerSellection;
+using ControllerSelection;
+using UnityEngine.Video;
 
 public class AFKManager : MonoBehaviour {
 
+	public delegate void videoChange(VideoClip clip);
+	public static videoChange OnVideoChange;
+
+	[SerializeField] float waitTime;
+	public VideoPlayer videoPlayer;	
+
 	private void OnEnable() {
-		
+		OVRManager.HMDUnmounted += OnTakeOff;
+		OVRManager.HMDMounted += HeadsetPutOn;
+		OnVideoChange += SetVideo;
 	}
 
 	private void OnDisable() {
-		
+		OnVideoChange -= SetVideo;
+	}
+
+	private void SetVideo(VideoClip clip){
+		videoPlayer.clip = clip;
+		videoPlayer.Prepare();
+		videoPlayer.Play();
+	}
+
+	private void OnTakeOff(){
+		// after 7 seconds reset 
+
+	}
+
+	private IEnumerator ResetProgress(){
+		yield return new WaitForSecondsRealtime(waitTime);
+		// reset to selection screen
+	}
+
+	private void HeadsetPutOn(){
+
 	}
 }
